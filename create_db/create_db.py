3d4827b6e974a923242d517bb002f4aec2c109da
SirSanewa <lukaszsanewski@gmail.com>
2020-12-21 18:47:05 +0100

    image download system update

--- create_db/create_db.py
+++ create_db/create_db.py
@@ -1,6 +1,4 @@
-import urllib.request
-import os
-
+import requests
 from selenium import webdriver
 from selenium.webdriver import ActionChains
 from selenium.webdriver.common.keys import Keys
@@ -74,50 +72,73 @@ def player_data(player_row) -> tuple:
     return player_name, player_position, player_shirt, player_dob, player_value
 
 
-def save_club_logo_to_file(driver):
-    club_image_object = driver.find_element_by_css_selector("#verein_head > div > div > div.dataBild > img")
-    image_src = club_image_object.get_attribute('src')
-    urllib.request.urlretrieve(image_src, "temp/temp.png")
+def get_image(driver, image_type=None, player_id=None):
+    """
+    Takes in ChromeDriver. If image_type and player_id not set then None by default.
+    Requests image from image source url and returns it as binary.
+    :param driver:
+    :param image_type:
+    :param player_id:
+    :return: binary image
+    """
+    if image_type == "player" and player_id:
+        selector = f"#yw1 > table > tbody > tr:nth-child({player_id}) > td.posrela > table > tbody > tr:nth-child(1) " \
+                   f"> td:nth-child(1) > a > img "
+        attribute = "data-src"
+    else:
+        selector = "#verein_head > div > div > div.dataBild > img"
+        attribute = "src"
+    image_object = driver.find_element_by_css_selector(selector)
+    url = image_object.get_attribute(attribute)
+    response = requests.get(url, stream=True)
+    return response.content
 
 
 def create_new_db_club(driver, club_id):
+    """
+    Creates new club in database.
+    :param driver:
+    :param club_id:
+    :return:
+    """
     name_select = "#verein_head > div > div.dataHeader.dataExtended > div.dataMain > div > div.dataName > h1 > span"
     full_club_name = driver.find_element_by_css_selector(name_select).text
-    with open("temp/temp.png", "rb") as file:
-        club_image = bytearray(file.read())
-        new_item = Club(club_id=club_id, club_name=full_club_name, club_logo=club_image)
-    os.remove("temp/temp.png")
+    club_image = get_image(driver)
+    new_item = Club(club_id=club_id, club_name=full_club_name, club_logo=club_image)
     db.add(new_item)
     db.commit()
 
 
-def save_player_photo_to_file(driver, player_id):
-    player_image_object = driver.find_element_by_xpath(
-        f"//*[@id='yw1']/table/tbody/tr[{player_id}]/td[2]/table/tbody/tr[1]/td[1]/a/img")
-    player_image_src = player_image_object.get_attribute('data-src')
-    urllib.request.urlretrieve(player_image_src, "temp/temp.png")
-
-
-def player_nationality(player):
+def player_nationality(player) -> str:
+    """
+    Gets player's nationality name from flag element title and returns it
+    :param player:
+    :return: nationality as string
+    """
     flag = player.find_element_by_class_name("flaggenrahmen")
     return flag.get_property("title")
 
 
-def create_new_db_player(driver, club_id):
+def create_new_db_player(driver, club_id, player_id):
+    """
+    Creates new player in database.
+    :param driver:
+    :param club_id:
+    :param player_id:
+    :return:
+    """
     player_name, player_position, player_shirt, player_dob, player_value = player_data(driver)
     nationality = player_nationality(driver)
-    with open("temp/temp.png", "rb") as file:
-        player_image = bytearray(file.read())
-        new_item = Player(name=player_name,
-                          photo=player_image,
-                          dob=player_dob,
-                          club_id=club_id,
-                          shirt_number=player_shirt,
-                          position=player_position,
-                          nationality=nationality,
-                          value=player_value
-                          )
-    os.remove("temp/temp.png")
+    player_image = get_image(driver, "player", player_id)
+    new_item = Player(name=player_name,
+                      photo=player_image,
+                      dob=player_dob,
+                      club_id=club_id,
+                      shirt_number=player_shirt,
+                      position=player_position,
+                      nationality=nationality,
+                      value=player_value
+                      )
     db.add(new_item)
     db.commit()
 
@@ -139,13 +160,11 @@ def data_mining(driver):
         link_club_name = get_club_link_name(club)
         use_link_and_switch_to_club_page(club, link_club_name, driver)
 
-        save_club_logo_to_file(driver)
         create_new_db_club(driver, club_id)
 
         players_table = driver.find_element_by_xpath("//*[@id='yw1']/table/tbody")
         for player_id, player in enumerate(players_table.find_elements_by_xpath("./tr"), start=1):
-            save_player_photo_to_file(player, player_id)
-            create_new_db_player(player, club_id)
+            create_new_db_player(player, club_id, player_id)
 
         driver.close()
         driver.switch_to.window(window_before)
